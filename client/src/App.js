91caae57fa763e7f434954df2afc4f7a1afcd226
Eric Nunez <ericnunez@gmail.com>
2020-11-25 12:14:02 -0600

    added a protected route for my profile page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import NotFound from "./views/NotFound/NotFound";
 import { setUser } from "./store/actions/auth.js";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import MyProfile from "./views/MyProfile/MyProfile";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 function App() {
   const dispatch = useDispatch();
@@ -19,6 +21,7 @@ function App() {
     <div className="App">
       <Header />
       <Switch>
+        <ProtectedRoute path="/my-profile" component={MyProfile} />
         <Route exact path="/search" component={SearchPage}></Route>
         <Route exact path="/not-found" component={NotFound}></Route>
         <Route exact path="/" component={Home}></Route>
