d2142170026f1de9e0b888b5d028eee037d945f5
Eric Nunez <ericnunez@gmail.com>
2020-11-28 23:12:36 -0600

    started the user profile section

--- client/src/views/MyProfile/MyProfile.jsx
+++ client/src/views/MyProfile/MyProfile.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import "./MyProfile.css";
+import { Container } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const MyProfile = () => {
-  return <div>My Profile</div>;
+  return (
+    <Container maxWidth="lg">
+      <div className="profile-grid">
+        <div>
+          <Typography variant="h3" gutterBottom>
+            Image here 1
+          </Typography>
+          <Typography variant="h4" gutterBottom>
+            Eric Nunez
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            User Bio should be here. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Corporis ipsam ullam, ducimus quaerat vero totam
+            provident nesciunt ad laboriosam illo voluptatem adipisci eaque?
+            Tenetur, explicabo? Perferendis qui molestias incidunt nobis.
+          </Typography>
+        </div>
+        <div>
+          <Typography variant="h3" gutterBottom>
+            Image here
+          </Typography>
+          <Typography variant="h4" gutterBottom>
+            Eric Nunez
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            User Bio should be here. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Corporis ipsam ullam, ducimus quaerat vero totam
+            provident nesciunt ad laboriosam illo voluptatem adipisci eaque?
+            Tenetur, explicabo? Perferendis qui molestias incidunt nobis.
+          </Typography>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default MyProfile;
